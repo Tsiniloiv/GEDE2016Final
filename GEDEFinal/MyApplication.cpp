f91c42bded5682531fc2f74bbfed928015ce7194
Tsiniloiv <jonb12@ru.is>
2016-03-29 22:21:13 +0000

    more raycast progress

--- GEDEFinal/MyApplication.cpp
+++ GEDEFinal/MyApplication.cpp
@@ -136,12 +136,30 @@ class MyApplication {
 
 			for(Ogre::Real i = farRightCorner.x; i > farLeftCorner.x; i -= resolution) {
 				for(Ogre::Real j = farRightCorner.z; j < nearRightCorner.z; j+= resolution) {
-					cout << "Placing marker at " << i << ", " << j << endl;
+					/*cout << "Placing marker at " << i << ", " << j << endl;
 					Ogre::Entity* ent = _sceneManager->createEntity("Sinbad.mesh");
 					Ogre::SceneNode* testNode = _sceneManager->getRootSceneNode()->createChildSceneNode();
 					testNode->attachObject(ent);
-					testNode->setPosition(i, 0, j);
+					testNode->setPosition(i, 0, j);*/
 				}
+			}
+			
+			Ogre::Vector3 raycastresult;
+
+			if(RaycastFromPoint(Ogre::Vector3(0, 5, 0), Ogre::Vector3(0, -1, 0), raycastresult)) {
+				cout << "Ray hit at " << raycastresult << endl;
+			} else {
+				cout << "Ray miss" << endl;
+			}
+
+			Ogre::Ray ray(Ogre::Vector3(0, 5, 0), Ogre::Vector3(0, -1, 0));
+
+			std::pair<bool, Ogre::Real> manualrayresult = ray.intersects(_sceneManager->getEntity("sinbad")->getBoundingBox());
+
+			if(manualrayresult.first) {
+				cout << "Manual ray hit at " << ray.getPoint(manualrayresult.second) << endl;
+			} else {
+				cout << "Manual ray miss" << endl;
 			}
 
 			_sceneManager->setAmbientLight(Ogre::ColourValue(0.6f,0.6f,0.6f));
@@ -162,6 +180,8 @@ class MyApplication {
 			// create the ray to test
 			Ogre::Ray ray(Ogre::Vector3(point.x, point.y, point.z),
 						  Ogre::Vector3(normal.x, normal.y, normal.z));
+
+			cout << "Ray origin: " << ray.getOrigin() << " Ray direction: " << ray.getDirection() << endl;
  
 			// check we are initialised
 			if (_ray_scene_query != NULL)
@@ -214,9 +234,9 @@ class MyApplication {
 					unsigned long *indices;
  
 					// get the mesh information
-				 OgreVE::GetMeshInformation(pentity->getMesh(), vertex_count, vertices, index_count, indices,             
-									  pentity->getParentNode()->getWorldPosition(),
-									  pentity->getParentNode()->getWorldOrientation(),
+				 GetMeshInformation(pentity->getMesh(), vertex_count, vertices, index_count, indices,             
+									  pentity->getParentNode()->_getDerivedPosition(),
+									  pentity->getParentNode()->_getDerivedOrientation(),
 									  pentity->getParentNode()->_getDerivedScale());
  
 					// test for hitting individual triangles on the mesh
@@ -266,4 +286,130 @@ class MyApplication {
 				return (false);
 			} 
 		}
-		};
+
+		// Get the mesh information for the given mesh.
+		// Code found on this forum link: http://www.ogre3d.org/wiki/index.php/RetrieveVertexData
+		void GetMeshInformation(const Ogre::MeshPtr mesh,
+                                size_t &vertex_count,
+                                Ogre::Vector3* &vertices,
+                                size_t &index_count,
+                                unsigned long* &indices,
+                                const Ogre::Vector3 &position,
+                                const Ogre::Quaternion &orient,
+                                const Ogre::Vector3 &scale)
+{
+    bool added_shared = false;
+    size_t current_offset = 0;
+    size_t shared_offset = 0;
+    size_t next_offset = 0;
+    size_t index_offset = 0;
+
+    vertex_count = index_count = 0;
+
+    // Calculate how many vertices and indices we're going to need
+    for (unsigned short i = 0; i < mesh->getNumSubMeshes(); ++i)
+    {
+        Ogre::SubMesh* submesh = mesh->getSubMesh( i );
+
+        // We only need to add the shared vertices once
+        if(submesh->useSharedVertices)
+        {
+            if( !added_shared )
+            {
+                vertex_count += mesh->sharedVertexData->vertexCount;
+                added_shared = true;
+            }
+        }
+        else
+        {
+            vertex_count += submesh->vertexData->vertexCount;
+        }
+
+        // Add the indices
+        index_count += submesh->indexData->indexCount;
+    }
+
+
+    // Allocate space for the vertices and indices
+    vertices = new Ogre::Vector3[vertex_count];
+    indices = new unsigned long[index_count];
+
+    added_shared = false;
+
+    // Run through the submeshes again, adding the data into the arrays
+    for ( unsigned short i = 0; i < mesh->getNumSubMeshes(); ++i)
+    {
+        Ogre::SubMesh* submesh = mesh->getSubMesh(i);
+
+        Ogre::VertexData* vertex_data = submesh->useSharedVertices ? mesh->sharedVertexData : submesh->vertexData;
+
+        if((!submesh->useSharedVertices)||(submesh->useSharedVertices && !added_shared))
+        {
+            if(submesh->useSharedVertices)
+            {
+                added_shared = true;
+                shared_offset = current_offset;
+            }
+
+            const Ogre::VertexElement* posElem =
+                vertex_data->vertexDeclaration->findElementBySemantic(Ogre::VES_POSITION);
+
+            Ogre::HardwareVertexBufferSharedPtr vbuf =
+                vertex_data->vertexBufferBinding->getBuffer(posElem->getSource());
+
+            unsigned char* vertex =
+                static_cast<unsigned char*>(vbuf->lock(Ogre::HardwareBuffer::HBL_READ_ONLY));
+
+            // There is _no_ baseVertexPointerToElement() which takes an Ogre::Real or a double
+            //  as second argument. So make it float, to avoid trouble when Ogre::Real will
+            //  be comiled/typedefed as double:
+            //      Ogre::Real* pReal;
+            float* pReal;
+
+            for( size_t j = 0; j < vertex_data->vertexCount; ++j, vertex += vbuf->getVertexSize())
+            {
+                posElem->baseVertexPointerToElement(vertex, &pReal);
+
+                Ogre::Vector3 pt(pReal[0], pReal[1], pReal[2]);
+
+                vertices[current_offset + j] = (orient * (pt * scale)) + position;
+            }
+
+            vbuf->unlock();
+            next_offset += vertex_data->vertexCount;
+        }
+
+
+        Ogre::IndexData* index_data = submesh->indexData;
+        size_t numTris = index_data->indexCount / 3;
+        Ogre::HardwareIndexBufferSharedPtr ibuf = index_data->indexBuffer;
+
+        bool use32bitindexes = (ibuf->getType() == Ogre::HardwareIndexBuffer::IT_32BIT);
+
+        unsigned long*  pLong = static_cast<unsigned long*>(ibuf->lock(Ogre::HardwareBuffer::HBL_READ_ONLY));
+        unsigned short* pShort = reinterpret_cast<unsigned short*>(pLong);
+
+
+        size_t offset = (submesh->useSharedVertices)? shared_offset : current_offset;
+
+        if ( use32bitindexes )
+        {
+            for ( size_t k = 0; k < numTris*3; ++k)
+            {
+                indices[index_offset++] = pLong[k] + static_cast<unsigned long>(offset);
+            }
+        }
+        else
+        {
+            for ( size_t k = 0; k < numTris*3; ++k)
+            {
+                indices[index_offset++] = static_cast<unsigned long>(pShort[k]) +
+                    static_cast<unsigned long>(offset);
+            }
+        }
+
+        ibuf->unlock();
+        current_offset = next_offset;
+    }
+}
+};
